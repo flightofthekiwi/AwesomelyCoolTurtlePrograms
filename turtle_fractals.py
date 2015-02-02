e0a319e14b80c1c59bed178cc43cd617bf05b414
flightofthekiwi <am2317@gmail.com>
2015-02-02 00:24:11 -0800

    Create turtle_fractals.py

---
+++ turtle_fractals.py
@@ -0,0 +1,32 @@
+# fractal triangles
+import turtle
+
+def drawFractalTriangle(aTurtle, depth, maxdepth):
+    if depth > maxdepth:
+        return
+    else:
+        for i in range(3):
+            aTurtle.forward(256/(2**depth))
+            drawFractalTriangle(aTurtle, depth+1, maxdepth)
+            aTurtle.forward(256/(2**depth))
+            aTurtle.left(120)
+        return
+
+def draw_picture():
+    window = turtle.Screen()
+    window.bgcolor("green")
+    brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.color('blue')
+    brad.penup()
+    brad.goto(-50, -200)
+    brad.pendown()
+    brad.hideturtle()
+    brad.speed(0)
+    brad.right(180)
+    brad.forward(256)
+    brad.right(180)
+    drawFractalTriangle(brad, 0, 8)
+    window.exitonclick()
+
+draw_picture()
